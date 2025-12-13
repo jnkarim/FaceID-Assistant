990f9f2bd2531b5919c065448bb62344336b0351
jnkarim <151433912+jnkarim@users.noreply.github.com>
2025-12-13 15:02:35 +0600

    update signup page design

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -5,7 +5,7 @@ export default function MainLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <main className="flex-1">{children}</main>
+      <main className="flex-1 ml-64">{children}</main>
     </div>
   );
 }
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -3,6 +3,7 @@
 import { Eye, EyeOff } from "lucide-react";
 import { useState } from "react";
 import Link from "next/link";
+import { Radar } from "lucide-react";
 
 interface Signup {
   firstName: string;
@@ -29,105 +30,125 @@ export default function Signup() {
   const handleSubmit = () => {};
 
   return (
-    <div className="min-h-screen bg-black flex items-center justify-center px-8">
-      <div className="w-full grid grid-cols-1 lg:grid-cols-2">
-        {/*Left Column- the form*/}
-        <div className="flex flex-col justify-center gap-2">
-          {/*Header*/}
-          <div className="text-white text-xl font-semibold">
-            FaceID Assistant
-          </div>
+    <div className="min-h-screen bg-black flex flex-col ">
+      <div className="w-full px-8 py-6 flex items-center justify-start border-b border-neutral-800 gap-8">
+        {/*Header*/}
 
-          {/*Navigate to Home*/}
-          <nav className="flex items-center gap-2">
-            <Link
-              href="/"
-              className="text-lg font-bold text-neutral-400 hover:text-lime-400 transition underline"
-            >
-              Home
-            </Link>
-          </nav>
+        <div className="gap-2 flex flex-row items-center">
+          <Radar className="w-6 h-6 text-lime-400" />
+          <div className="text-white text-xl font-bold">FaceID Assistant</div>
+        </div>
 
-          <div className="flex flex-row">
-            <h1 className="text-3xl font-bold text-white">Already a member?</h1>
+        {/*Navigate to Home*/}
+        <nav className="flex items-center gap-2">
+          <Link
+            href="/"
+            className="text-lg font-bold text-neutral-400 hover:text-lime-400 transition"
+          >
+            Home
+          </Link>
+        </nav>
+      </div>
 
-            <Link
-              href="/login"
-              className="text-lime-400 hover:text-lime-300 transition font-medium underline"
-            >
-              Log in
-            </Link>
-          </div>
-
-          {/*Form*/}
-          <form onSubmit={handleSubmit} className="space-y-5">
-            {/*first name & last name*/}
-            <div className="flex flex-row justify-between gap-4">
-              {" "}
-              <input
-                name="firstName"
-                type="text"
-                placeholder="First Name"
-                value={formData.firstName}
-                onChange={handleChange}
-                className="w-full bg-transparent px-6 py-4 text-white border border-neutral-400 focus:border-lime-400 rounded-xl transition placeholder-neutral-500 focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
-              />
-              <input
-                name="lastName"
-                type="text"
-                placeholder="Last Name"
-                value={formData.lastName}
-                onChange={handleChange}
-                className="w-full bg-transparent px-6 py-4 text-white border border-neutral-400 focus:border-lime-400 rounded-xl transition placeholder-neutral-500 focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
-              />
+      <div className="flex-1 flex px-8">
+        <div className="w-full grid grid-cols-1 lg:grid-cols-2">
+          {/*Left Column- the form*/}
+          <div className="flex flex-col justify-center gap-2">
+            <div className="text-xl font-semibold text-neutral-400">
+              START FOR FREE
             </div>
-            {/*email*/}
-            <input
-              name="email"
-              type="email"
-              placeholder="Email"
-              value={formData.email}
-              onChange={handleChange}
-              className="w-full bg-transparent px-6 py-4 text-white border border-neutral-400 focus:border-lime-400 transition rounded-xl placeholder-neutral-600 focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
-            />
-            {/*password*/}
-            <div className="relative">
+            <div className="flex flex-row items-start gap-2">
+              {" "}
+              <div className="text-5xl font-semibold">Create new account</div>
+              <span className="mt-8 w-2 h-2 bg-lime-400 rounded-full" />
+            </div>
+
+            <div className="flex flex-row items-center gap-2">
+              <h1 className="text-lg font-bold text-neutral-400">
+                Already a member?
+              </h1>
+
+              <Link
+                href="/login"
+                className="text-lime-400 hover:text-lime-300 transition font-medium underline"
+              >
+                Log in
+              </Link>
+            </div>
+
+            {/*Form*/}
+            <form onSubmit={handleSubmit} className="space-y-5">
+              {/*first name & last name*/}
+              <div className="flex flex-row justify-between gap-4">
+                {" "}
+                <input
+                  name="firstName"
+                  type="text"
+                  placeholder="First Name"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                  className="w-full bg-transparent px-6 py-4 text-white border border-neutral-400 focus:border-lime-400 rounded-xl transition placeholder-neutral-500 focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
+                />
+                <input
+                  name="lastName"
+                  type="text"
+                  placeholder="Last Name"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                  className="w-full bg-transparent px-6 py-4 text-white border border-neutral-400 focus:border-lime-400 rounded-xl transition placeholder-neutral-500 focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
+                />
+              </div>
+              {/*email*/}
               <input
-                name="password"
-                type={showPassword ? "text" : "password"}
-                placeholder="********"
-                value={formData.password}
+                name="email"
+                type="email"
+                placeholder="Email"
+                value={formData.email}
                 onChange={handleChange}
                 className="w-full bg-transparent px-6 py-4 text-white border border-neutral-400 focus:border-lime-400 transition rounded-xl placeholder-neutral-600 focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
               />
-              <button
-                type="button"
-                onClick={() => setShowPassword(!showPassword)}
-                className="absolute right-4 top-1/2 -translate-y-1/2 text-neutral-400 hover:text-lime-400 transition disabled:opacity-50"
-              >
-                {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
-              </button>
-            </div>
+              {/*password*/}
+              <div className="relative">
+                <input
+                  name="password"
+                  type={showPassword ? "text" : "password"}
+                  placeholder="********"
+                  value={formData.password}
+                  onChange={handleChange}
+                  className="w-full bg-transparent px-6 py-4 text-white border border-neutral-400 focus:border-lime-400 transition rounded-xl placeholder-neutral-600 focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute right-4 top-1/2 -translate-y-1/2 text-neutral-400 hover:text-lime-400 transition disabled:opacity-50"
+                >
+                  {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                </button>
+              </div>
 
-            {/*Submit Button*/}
-            <div className="pt-2">
-              {" "}
-              <button
-                type="submit"
-                className="w-full px-8 py-4 bg-lime-400 text-black rounded-xl hover:bg-lime-300 transition font-semibold text-lg disabled:opacity-50 disabled:cursor-not-allowed"
-              >
-                Create Account
-              </button>
-            </div>
+              {/*Submit Button*/}
+              <div className="pt-2">
+                {" "}
+                <button
+                  type="submit"
+                  className="w-full px-8 py-4 bg-lime-400 text-black rounded-xl hover:bg-lime-300 transition font-semibold text-lg disabled:opacity-50 disabled:cursor-not-allowed"
+                >
+                  Create Account
+                </button>
+              </div>
 
-            <p className="text-neutral-500 text-sm">
-              By creating an account, you agree to our Terms of Service and
-              Privacy Policy
-            </p>
-          </form>
+              <p className="text-neutral-500 text-sm">
+                By creating an account, you agree to our Terms of Service and
+                Privacy Policy
+              </p>
+            </form>
+          </div>
         </div>
+
         {/*Right Column- image*/}
-        <div className="flex flex-col justify-center"></div>
+        <div className="hidden lg:flex justify-end items-end p-4">
+          <Radar className="w-8 h-8 text-lime-400" />
+        </div>
       </div>
     </div>
   );
